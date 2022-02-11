19304cc6d2a709c46583f696a67007172c51c002
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-02-11 15:19:06 +0100

    add package and readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# SARI Sparql Parser
+
+A parser for SPARQL queries and updates
+
+## Installation
+
+install using pip
+
+
+```sh
+pip install sari-sparql-parser
+```
+
+## Usage
+
+Parse a SPARQL query:
+
+```python
+query = """PREFIX crm: <http://www.cidoc-crm.org/cidoc/crm>
+    SELECT ?subject ?identifier WHERE {
+        ?subject crm:P1_is_identified_by ?identifier .
+        ?identifier a ?type .
+        VALUES (?type) {
+            (crm:E41_Appellation)
+            (crm:E42_Identifier)
+        }
+    }
+"""
+from sariSparqlParser import parser
+p = parser()
+p.parseQuery(query)
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="sari-sparql-parser",
+    version="0.0.1",
+    author="Florian Kräutli",
+    author_email="florian.kraeutli@uzh.ch",
+    description="A library for parsing SPARQL queries and updates",
+    include_package=True,
+    install_requires=['rdflib','pyparsing'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/swiss-art-research-net/sari-sparql-parser.git",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
